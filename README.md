a393a66e668ba9100652f8d19f250ec8b2091d10
ZappeL <zappel@simple-co.de>
2016-03-20 18:13:52 +0100

    several additions...

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # sf_installer
+
+####Installation (as root):
+```sh
+    cd /opt && git clone https://github.com/aspann/sf_installer
+    ln -sf /opt/sf_installer/install_sf.sh /usr/local/bin/install_sf-2.8
+    ln -sf /usr/local/bin/install_sf-2.8 /usr/local/bin/install_sf
+```
+
+####Usage:
+```
+    install_sf[.sh] <project_name> <options>
+        options:
+         -f force overwrite
+         -h help
+         -y Yes to all questions
+```
--- install_sf.sh
+++ install_sf.sh
@@ -19,26 +19,61 @@ gICAgICAgICAgICAgZHJpdmVyOiAgIHBkb19zcWxpdGUNCiAgICAgICAgICAgICAgICBwYXRoOiAgI\
 CAgJyVrZXJuZWwuY2FjaGVfZGlyJS9kYXRhLnNxbGl0ZSc="
 
 ## Welcome message
-prntMessage "=== Symfony Framework installer (2.8) ==="
-echo -e ""
+prntMessage "=== Symfony Framework installer (2.8) ===\n"
+
+function help {
+    prntMessage "usage: $0 <project_name> <options>\n\
+        options:\n\
+         -f\tforce overwrite\n\
+         -h\thelp$1\n\
+         -y\tYes to all questions"
+}
 
 ## checking if $1 (project_name) is set
 if [[ -z $1 ]]; then
-    prntMessage "usage: $0 <project_name> <options>\noptions:\n -f\tforce overwrite"
+    help " (this message)"
     exit 0;
 else
-    project_name=$1
-    if [[ $2 == "-f" ]] && [[ -d ${project_name} ]]; then
-        prntWithSpaces "Deleting existing project: '${project_name}' (due -f option)..."
+    ## parsing options..
+    while [[ $# -gt 0 ]]; do
+        case "$1" in
+            -h|--h|--help)
+                help " (this message)"; exit 0; ;;
+            -f)
+                opt_force=true; ;;
+            -y)
+                opt_y2all=true; ;;
+            -fy|-yf)
+                opt_force=true
+                opt_y2all=true
+                ;;
+            *)
+                if [[ ! ${project_name} ]] && [[ ! $1 == -* ]]; then
+                    project_name=$1
+                else
+                    prntMessage ">>> Invalid option: $1" "err"; echo
+                    help; exit 0
+                fi
+        esac
+        shift
+    done
+
+    ## checking paths... (existence)
+    if [[ -d ${project_name} ]] && [[ ${opt_force} ]]; then
+        prntWithSpaces "Deleting existing project '${project_name}' due -f option..."
         rm -rf ${project_name}
         if [[ -d ${project_name} ]]; then
             prntERR; exit 1;
         fi
         prntOK
-        echo -e ""
     elif [[ -d ${project_name} ]]; then
-        prntCRIT "Sorry, project ($1) exists already."
+        prntCRIT "Sorry, project '${project_name}' exists already."
     fi
+
+    if [[ ${opt_y2all} ]]; then
+        prntWithSpaces "Enabeling 'yes to all' due -y option..."
+        prntOK
+    fi; echo
 fi
 
 ## checking for binaries (php, composer, npm, bower, symfony)
@@ -55,10 +90,9 @@ else
         | awk -v FS="(composer=| alias=)" '{print $2}') | tr -d '[[:space:]]')
         composer=${composer:1:-1}
     fi
-fi
-echo -e ""
+fi; echo
 
-## preparing symfony base-installlation...
+## preparing symfony base-installation...
 prntWithSpaces "Installing Symfony 2.8..."
 symfony new ${project_name} 2.8 >/dev/null 2>&1
 if [[ -d ${project_name} ]]; then
@@ -66,23 +100,31 @@ if [[ -d ${project_name} ]]; then
 
     ## installing bundle files
     for bundle in ${installer_path}/mod_bundle/*.bundle; do
-        source ${bundle};
+        if [[ ! ${opt_y2all} ]]; then
+            read -p "Install $(basename $bundle | \
+             sed -s 's/.bundle//g')? (y/n): " -n 1 -r
+            echo
+        else
+            REPLY="y"
+        fi
+
+        if [[ $REPLY =~ ^[YyJj]$ ]]; then
+            source ${bundle};
+        fi
     done
 
     ## finalizing install (comperser update)
     prntWithSpaces "Finalizing Symfony 2.8 (composer update)..."
     $(cd ${project_name} && eval ${composer} update >/dev/null 2>&1)
-    prntOK
-
-    echo -e ""
-    prntWithSpaces "Fixing test-environment..."
+    prntOK; echo
     ## patching config_test.yml
+    prntWithSpaces "Fixing test-environment..."
     echo $CONFTST_YML_B64 | base64 -d >> ${project_name}/app/config/config_test.yml
     prntOK
 else
     prntCRIT "Sorry, can't install Symfony 2.8."
 fi
-echo -e ""
+echo
 
 prntMessage "Symfony 2.8 setup complete."
 
